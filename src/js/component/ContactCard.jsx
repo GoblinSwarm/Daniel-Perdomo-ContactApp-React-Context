77acaa5cc9fba84a46791084c5cf5d0597331ba6
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-07-05 21:50:16 +0000

    Added classes and a basic not working structure

---
+++ src/js/component/ContactCard.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+/*
+    COMO USAR EL STORE
+    1.- Importar el hook que manejara el store (useContext)
+    2.- Importar el contexto (AppContext)
+    3.- implementarlo en el componente (con useContext)
+    4.- usar el store o actions
+*/
+
+export const Navbar = () => {
+    const { store, actions } = useContext(Context)
+    const { user } = store
+
+    console.log(user.name)
+
+    return (
+            <div className="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+    )
+    
+};
--- src/js/layout.js
+++ src/js/layout.js
@@ -2,13 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
+import { Contact } from "./views/Contact";
+import { AddContact } from "./views/AddContact";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import injectContext from "./store/appContext";
 
 //create your first component
 const Layout = () => {
@@ -20,14 +17,10 @@ const Layout = () => {
 		<div>
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Navbar />
 					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
+						<Route path="/" element={<Contact />} />
+						<Route path="/demo" element={<AddContact />} />
 					</Routes>
-					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
